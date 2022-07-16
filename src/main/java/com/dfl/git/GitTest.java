b348604e7abfff2b4b2d2c8953e16f13d05ce527
dfl <dfl@102.com>
2022-07-16 21:30:39 +0800

    first commit

---
+++ src/main/java/com/dfl/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.dfl.git;
+
+/**
+ * @title: GitTest
+ * @Author: DuDo
+ * @Date: 2022/7/16 21:24
+ * @Version: 1.8
+ * @Description:
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
