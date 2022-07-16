127a733fd1f72641ef79492bfa8dd15d8a1e389d
dfl <dfl@102.com>
2022-07-16 23:24:43 +0800

    third commit

--- src/main/java/com/dfl/git/GitTest.java
+++ src/main/java/com/dfl/git/GitTest.java
@@ -16,5 +16,7 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("master test");
         System.out.println("hot-fix commit");
+        System.out.println("push test");
+
     }
 }
